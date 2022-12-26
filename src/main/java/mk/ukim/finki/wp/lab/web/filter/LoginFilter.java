940553fe228e3a943cffbf1732486f5969664b2c
Aleksandra <aleftimova@gmail.com>
2022-12-26 14:29:58 +0100

    lab - update

---
+++ src/main/java/mk/ukim/finki/wp/lab/web/filter/LoginFilter.java
@@ -0,0 +1,39 @@
+package mk.ukim.finki.wp.lab.web.filter;
+
+import org.apache.catalina.User;
+import org.springframework.context.annotation.Profile;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+@Profile("servlet")
+public class LoginFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException{
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        User user = (User)request.getSession().getAttribute("user");
+
+        String path = request.getServletPath();
+
+        if (!"/login".equals(path) && !"/h2".equals(path) && !"/register".equals(path) && user==null){
+            response.sendRedirect("/login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
